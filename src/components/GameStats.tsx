4e3bf92f5d5beadb404ca32f1d7e2b4b803fa050
Theo Tillberg <ttillberg@gmail.com>
2022-03-07 14:50:44 +0100

    Fix formatting issues

--- src/components/GameStats.tsx
+++ src/components/GameStats.tsx
@@ -1,15 +1,20 @@
 import { useLupiContract } from "src/hooks/useLupiContract";
 import { Box, Grid, Text } from "src/ui";
+import { ethers, utils } from "ethers";
 
 export const GameStats = () => {
-  const { guessHashes, currentBalance, rolloverBalance, phaseDeadline } =
+  const { guessHashes, currentBalance, rolloverBalance, phaseDeadline, phase } =
     useLupiContract();
+
+  const wei = currentBalance?.add(rolloverBalance ?? 0);
+  const eth = wei && ethers.utils.formatEther(wei);
+
   return (
     <Grid columns={2} gap="sm" alignContent="center" alignItems="center">
       <Box padding="sm" border centerContent gap="xs">
         <Text header="small">Current Jackpot</Text>
         <Text header="regular" color="primary">
-          {currentBalance?.add(rolloverBalance ?? 0).toString()} eth
+          {eth} eth
         </Text>
       </Box>
       <Box padding="sm" gap="xs" border centerContent>
@@ -22,11 +27,17 @@ export const GameStats = () => {
         <Text header="small">The LUPI Reveal</Text>
         <Text header="regular" color="primary">
           {new Date(
-            Date.now() + (phaseDeadline?.toNumber() ?? 0)
+            Date.now() + (phaseDeadline?.toNumber() ?? 0) * 1000
           ).toLocaleTimeString()}
         </Text>
         <Text header="regular" color="primary">
           {phaseDeadline?.toString()}
+        </Text>
+      </Box>
+      <Box padding="sm" border centerContent gap="xs" cols={2}>
+        <Text header="small">Phase</Text>
+        <Text header="regular" color="primary">
+          {phase}
         </Text>
       </Box>
     </Grid>
