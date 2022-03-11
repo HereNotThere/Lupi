47dff1bf2aeb4a83964ff68f13efc83bfd886efd
Theo Tillberg <ttillberg@gmail.com>
2022-03-11 14:39:33 +0100

    Add network error + suggestions

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import { useCallback, useMemo, useState } from "react";
 import { DebugPanel } from "./components/DebugPanel";
 import { MenuId, SiteHeader } from "./components/SiteHeader";
-import { Box } from "./ui";
+import { Box, Button, Grid, Text } from "./ui";
 import { HowToPlayView } from "./views/HowToPlayPopup";
 import { PastGames } from "./views/PastGames";
 import { GamePhases } from "./views/GamePhases";
 import { LupiContractProvider, supportedChain } from "./hooks/useLupiContract";
 import { useWeb3Context, WalletStatus } from "./hooks/useWeb3";
+import { TextButton } from "./ui/Button/Button";
+import { BigGreyButton } from "./components/Buttons";
 
 function App() {
   const [pageId, setPageId] = useState<MenuId>("current-game");
   const [popup, setPopup] = useState(false);
   const { chainId, walletStatus } = useWeb3Context();
   const isChainSupported = useMemo(() => supportedChain(chainId), [chainId]);
+  const { addArbitrumRinkebyChain, addArbitrumOneChain } = useWeb3Context();
 
   const onSelectMenuItem = useCallback((menuId: string) => {
     switch (menuId) {
@@ -53,7 +56,26 @@ function App() {
             {popup ? <HowToPlayView onClose={onPopupClose} /> : <></>}
           </LupiContractProvider>
         ) : (
-          <>Unsupported chain</>
+          <Box grow centerContent>
+            <Box gap="lg" centerContent>
+              <Box centerContent>
+                <Text color="secondary" header="large">
+                  Unsupported chain
+                </Text>
+                <Text color="muted">
+                  Please switch to one of the supported networks
+                </Text>
+              </Box>
+              <Grid columns={2} gap="lg">
+                <BigGreyButton onClick={addArbitrumOneChain}>
+                  Arbitrum
+                </BigGreyButton>
+                <BigGreyButton onClick={addArbitrumRinkebyChain}>
+                  Arbitrum Rinkeby
+                </BigGreyButton>
+              </Grid>
+            </Box>
+          </Box>
         )}
       </Box>
     </Box>
