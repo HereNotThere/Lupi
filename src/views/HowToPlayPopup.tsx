03b5998db78358cae647b764ad53f6e2c4322297
Theo Tillberg <ttillberg@gmail.com>
2022-03-14 11:55:18 +0100

    Fix how-to-play position for mobile

--- src/views/HowToPlayPopup.tsx
+++ src/views/HowToPlayPopup.tsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 import { CloseIcon } from "src/components/Icons";
+import { useResponsive } from "src/hooks/useResponsive";
 import { Box, Button, Text } from "src/ui";
 import styled from "styled-components";
 
@@ -29,72 +30,86 @@ const animateProps = {
   exit: "hide",
 };
 
-export const HowToPlayView = (props: { onClose?: () => void }) => (
-  <MotionBox
-    fillSpace
-    centerContent
-    onClick={props.onClose}
-    variants={backgroundVariants}
-    {...animateProps}
-    transition={{ duration: 0.4 }}
-    style={{ zIndex: 100 }}
-  >
-    <MotionBox
-      maxWidth={650}
-      background="muted"
-      padding="xl"
-      borderRadius="sm"
-      gap="md"
+export const HowToPlayView = (props: { onClose?: () => void }) => {
+  const { isSmall } = useResponsive();
+  return (
+    <FixedContainer
+      centerContent
+      onClick={props.onClose}
+      variants={backgroundVariants}
       {...animateProps}
-      variants={popupVariants}
-      transition={{ scale: { duration: 0.4 } }}
+      transition={{ duration: 0.4 }}
+      style={{ zIndex: 100 }}
+      padding="md"
     >
-      <CloseButton background="none" color="text" border={false}>
-        <CloseIcon />
-      </CloseButton>
-      <Text header="regular">Guess the LUPI</Text>
-      <Text>The winner is the person who guesses the number that is the:</Text>
-      <Text>
-        Lowest:{" "}
-        <Text span color="muted">
-          Thelowest guess that also fulfills the other requirements
+      <MotionBox
+        maxWidth={650}
+        background="muted"
+        padding={isSmall ? "lg" : "xl"}
+        borderRadius="sm"
+        gap="md"
+        {...animateProps}
+        variants={popupVariants}
+        transition={{ scale: { duration: 0.4 } }}
+      >
+        <CloseButton background="none" color="text" border={false}>
+          <CloseIcon />
+        </CloseButton>
+        <Text header="regular">Guess the LUPI</Text>
+        <Text>
+          The winner is the person who guesses the number that is the:
         </Text>
-      </Text>
-      <Text>
-        Unique:{" "}
-        <Text span color="muted">
-          Others must not have also guessed your number
+        <Text>
+          Lowest:{" "}
+          <Text span color="muted">
+            Thelowest guess that also fulfills the other requirements
+          </Text>
         </Text>
-      </Text>
-      <Text>
-        Positive:{" "}
-        <Text span color="muted">
-          Must be positive, not negative{" "}
+        <Text>
+          Unique:{" "}
+          <Text span color="muted">
+            Others must not have also guessed your number
+          </Text>
         </Text>
-      </Text>
-      <Text>
-        Integer:{" "}
-        <Text span color="muted">
-          Must be a whole number (ie 1, 3, 5){" "}
+        <Text>
+          Positive:{" "}
+          <Text span color="muted">
+            Must be positive, not negative{" "}
+          </Text>
         </Text>
-      </Text>
-      <Text>
-        Make sure to save your entry ticket after your submission in case you
-        change browsers or we lose your guess.{" "}
-      </Text>
-      <Text>
-        Entry fee is 0.01 ETH + gas. 100% of the entry fee goes to the jackpot.
-      </Text>
-      <Text>
-        After the guessing period is over, there will be a ticket checking
-        period. The person who is the LUPI once the ticket checking period is
-        over will receive the jackpot directly to their wallet.
-      </Text>
-    </MotionBox>
-  </MotionBox>
-);
+        <Text>
+          Integer:{" "}
+          <Text span color="muted">
+            Must be a whole number (ie 1, 3, 5){" "}
+          </Text>
+        </Text>
+        <Text>
+          Make sure to save your entry ticket after your submission in case you
+          change browsers or we lose your guess.{" "}
+        </Text>
+        <Text>
+          Entry fee is 0.01 ETH + gas. 100% of the entry fee goes to the
+          jackpot.
+        </Text>
+        <Text>
+          After the guessing period is over, there will be a ticket checking
+          period. The person who is the LUPI once the ticket checking period is
+          over will receive the jackpot directly to their wallet.
+        </Text>
+      </MotionBox>
+    </FixedContainer>
+  );
+};
 
 const MotionBox = motion(Box);
+
+const FixedContainer = styled(MotionBox)`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+`;
 
 const CloseButton = styled(Button)`
   position: absolute;
